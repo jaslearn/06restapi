023af41ebc4b89115ed9664ea1ed262c2cf500d9
jaslearn <jasdhir.learn@gmail.com>
2021-07-03 22:11:31 +0530

    First

---
+++ src/main/java/com/revature/dj/rest/MyClass.java
@@ -0,0 +1,25 @@
+package com.revature.dj.rest;
+
+public class MyClass {
+
+	private String name;
+
+	public MyClass() {
+		super();
+		this.name="Jasdhir";
+	}
+
+	public MyClass(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
